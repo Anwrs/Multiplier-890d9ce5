8a9e763b4fd8fd7a2e7aa550995746d42cd848eb
Anouar ElGhazaoui <2080049@talnet.nl>
2020-01-14 14:59:45 +0100

    Uitgevoerd

---
+++ multiplier.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Ik ga twee getallen elkaar vermenigvuldigen". PHP_EOL ;
+echo "Eerste getal?". PHP_EOL ;
+$a = readline("") ;
+echo "Tweede getal?". PHP_EOL ;
+$b = readline(""); 
+
+$c = $a + $b;
+
+echo $c;
+
+// echo "Uw resultaat is:" $a = ($b = $b) +  5";
